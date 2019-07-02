1cafbc44e3bb9f0f9cbe6ea8b55b4d17e6f060e9
Bacchus <47571329+BacchusJ@users.noreply.github.com>
2019-07-01 21:03:24 -0500

    interview questions added
    and returning the functions

--- script.js
+++ script.js
@@ -8,10 +8,26 @@ function interviewQuestion(job) {
     } else if (job === 'teacher') {
         return function(name) {
             console.log('What subject do you teach, ' + name + '?'); 
-        }
-    } else {
+        };
+
+    } else if (job === 'drag queen') {
+        return function(name) {
+            console.log(name + ' Who is your favorite artist to lipsync to?')
+        };
+    }
+    
+    else {
         return function(name) {
         console.log('Hello ' + name + ', what do you do?');
         }
     }
 }
+
+var teacheerQuestion = interviewQuestion('teacher');
+teacheerQuestion('Bacchus');
+
+var designerQuestion = interviewQuestion('designer');
+designerQuestion('Justin');
+
+var dragqueenQuestion = interviewQuestion('drag queen');
+dragqueenQuestion('RuPaul');
