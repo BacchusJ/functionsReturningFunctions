55c5199730cf7490f2a6484b61cc26669330a56c
Bacchus <47571329+BacchusJ@users.noreply.github.com>
2019-07-02 20:00:05 -0500

    using IIFE
    Immediately Invoked Functions Expressions
    
    Placed the code inside IIFE which gives the code privacy in case other developers use the same variables outside this code. They will not interfere

--- script.js
+++ script.js
@@ -1,35 +1,40 @@
 //function returning functions
 
-function interviewQuestion(job) {
-    if (job === 'designer') {
-        return function(name) {
-            console.log(name + ', can you please explaoin what UX design is?')
+(function () {
+    function interviewQuestion(job) {
+        if (job === 'designer') {
+            return function(name) {
+                console.log(name + ', can you please explaoin what UX design is?')
+            }
+        } else if (job === 'teacher') {
+            return function(name) {
+                console.log('What subject do you teach, ' + name + '?'); 
+            };
+    
+        } else if (job === 'drag queen') {
+            return function(name) {
+                console.log(name + ' Who is your favorite artist to lipsync to?')
+            };
         }
-    } else if (job === 'teacher') {
-        return function(name) {
-            console.log('What subject do you teach, ' + name + '?'); 
-        };
-
-    } else if (job === 'drag queen') {
-        return function(name) {
-            console.log(name + ' Who is your favorite artist to lipsync to?')
-        };
+        
+        else {
+            return function(name) {
+            console.log('Hello ' + name + ', what do you do?');
+            }
+        }
     }
     
-    else {
-        return function(name) {
-        console.log('Hello ' + name + ', what do you do?');
-        }
-    }
-}
+    var teacheerQuestion = interviewQuestion('teacher');
+    teacheerQuestion('Bacchus');
+    
+    var designerQuestion = interviewQuestion('designer');
+    designerQuestion('Justin');
+    
+    var dragqueenQuestion = interviewQuestion('drag queen');
+    dragqueenQuestion('RuPaul');
+    
+    interviewQuestion('drag queen')('Kika');
+    
+}) ()
 
-var teacheerQuestion = interviewQuestion('teacher');
-teacheerQuestion('Bacchus');
 
-var designerQuestion = interviewQuestion('designer');
-designerQuestion('Justin');
-
-var dragqueenQuestion = interviewQuestion('drag queen');
-dragqueenQuestion('RuPaul');
-
-interviewQuestion('drag queen')('Kika');
